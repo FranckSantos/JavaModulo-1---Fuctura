c3a9342a03f3c77ccfeaf7966264b451cc80b4f2
Francilvan Santos <42840188+FranckSantos@users.noreply.github.com>
2022-08-08 23:29:42 -0300

    Add files via upload

---
+++ Atividade 4/Ex2.java
@@ -0,0 +1,13 @@
+/* 3) Imprima a soma de todos os números de 1 até 100. */
+
+public class Soma1a100 {
+    public static void main(String[] args) {
+        int soma = 0;
+        int i = 1;
+        while (i <= 100){
+            soma = soma + i;
+            i++;
+        }
+        System.out.println("Soma dos números de 1 até 100: " + soma);
+    }
+}
